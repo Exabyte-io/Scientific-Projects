4fb12785efa6335bb44db77289f7844bf1ec3f9b
James Dean <24254612+AcylSilane@users.noreply.github.com>
2021-09-23 16:42:37 -0700

    Update noteboos

--- DigitalEcosystem/exploratory/exfoliation/JoulesMeterSquare.py
+++ DigitalEcosystem/exploratory/exfoliation/JoulesMeterSquare.py
@@ -240,3 +240,181 @@ DigitalEcosystem.utils.figures.save_parity_plot(train_x_regression[train_y_regre
                                                 target_column,
                                                 "exfoliation_joules_per_meter_lessThan2.jpeg")
 
+
+# In[]:
+
+
+n_importances = 10
+importances = list(zip(best_regression[1].feature_importances_, xenonpy_matminer_descriptors))
+
+sorted_importances = list(sorted(importances, key=lambda i: -i[0]))
+
+
+x=10
+plt.rcParams['figure.figsize'] = (2*x,x)
+plt.barh(range(n_importances), [imp[0] for imp in sorted_importances[:n_importances]])
+plt.yticks(range(n_importances), [imp[1] for imp in sorted_importances[:n_importances]])
+plt.ylabel("Feature")
+plt.xlabel("Importance Score")
+plt.tight_layout()
+plt.savefig("Importances.jpeg")
+
+
+# In[]:
+
+
+cols_for_sisso = [i[1] for i in sorted_importances][:10] + ['exfoliation_energy (J/m^2)']
+train[cols_for_sisso].to_csv('joules_meter_square_sisso_train.csv', index=False)
+test[cols_for_sisso].to_csv('joules_meter_square_sisso_test.csv', index=False)
+
+
+# # SISSO
+
+# In[]:
+
+
+models = {
+    'r1_1term': lambda df: 4.404873233341312e-01 + \
+                           9.781965911161868e-02 * (df['min:bulk_modulus'] / df['max:en_allen']),
+    'r1_2term': lambda df: 5.362243384327817e-01 + \
+                           -1.798113641923983e-01 * np.sin(df['min:bulk_modulus']) + \
+                           8.823903699275686e-02 * (df['min:bulk_modulus'] / df['max:en_allen']),
+    'r1_3term': lambda df: 6.016831442338191e-01 + \
+                           -2.927241001063905e-04 * (df['max:icsd_volume'] * df['min:bulk_modulus']) + \
+                           -1.985383974054647e-01 * np.sin(df['min:bulk_modulus']) + \
+                           1.860876547833066e-01 * (df['min:bulk_modulus'] / df['max:en_allen']),
+    'r1_4term': lambda df: 5.705073925771592e-01 + \
+                           -2.863198886084749e-04 * (df['max:icsd_volume'] * df['min:bulk_modulus']) + \
+                           -1.082323743327636e-01 * np.cos(df['max:en_allen']) + \
+                           -2.197625832041794e-01 * np.sin(df['min:bulk_modulus']) + \
+                           1.972152349576783e-01 * (df['min:bulk_modulus'] / df['max:en_allen']),
+    'r2_1term': lambda df: 5.028827080746997e-01 + \
+                           3.114625532016955e+02 * ((df['min:bulk_modulus'] / df['max:icsd_volume']) / (df['max:en_allen']**3)),
+    'r2_2term': lambda df: 1.024868520318417e+00 + \
+                           -4.430757799985536e-13 * ((df['max:mendeleev_number']**6) * np.sqrt(df['min:Polarizability'])) + \
+                           2.553498564455721e+02 * ((df['min:bulk_modulus'] / df['max:icsd_volume']) / (df['max:en_allen']**3)),
+    'r2_3term': lambda df: 1.007585809890319e+00 + \
+                           1.453137511190895e-01 * np.cos(df['var:en_pauling'] + df['min:bulk_modulus']) + \
+                           -4.118193819656038e-13 * ((df['max:mendeleev_number']**6) * np.sqrt(df['min:Polarizability'])) + \
+                           2.578671165508385e+02 * ((df['min:bulk_modulus'] / df['max:icsd_volume']) / (df['max:en_allen']**3)),
+    'r2_4term': lambda df: 9.633615867872918e-01 + \
+                           1.660922813669548e-01 * (np.exp(-1 * df['var:covalent_radius_pyykko_double']) / np.log(df['min:Polarizability'])) + \
+                           2.096670377386976e-03 * ((df['var:en_pauling'] * df['min:Polarizability']) / np.cos(df['min:bulk_modulus'])) + \
+                           -4.136201377618091e-13 * ((df['max:mendeleev_number']**6) * np.sqrt(df['min:Polarizability'])) + \
+                           2.647536423788877e+02 * ((df['min:bulk_modulus'] / df['max:icsd_volume']) / (df['max:en_allen']**3))
+}
+
+
+for key, fun in models.items():
+    train[key] = fun(train)
+    test[key] = fun(test)
+
+
+# In[]:
+
+
+target='exfoliation_energy (J/m^2)'
+train_min = min(train[target].min(), best_regression.predict(train_x_regression).min())
+train_max = max(train[target].min(), best_regression.predict(train_x_regression).max())
+test_min = min(test[target].min(), best_regression.predict(test_x_regression).min())
+test_max = max(test[target].min(), best_regression.predict(test_x_regression).max())
+
+for key in models.keys():
+    train_min = min(train_min, train[key].min())
+    train_max = max(train_max, train[key].max())
+    test_min = min(test_min, test[key].min())
+    test_max = max(test_max, test[key].max())
+
+def get_r2(label, dataset):
+    r2 = sklearn.metrics.r2_score(y_true=dataset[target], y_pred=dataset[label])
+    return np.round(r2, 2)
+
+
+# In[]:
+
+
+plt.rcParams['figure.figsize'] = (7,7)
+plt.rcParams['figure.dpi'] = 200
+plt.rcParams['font.size'] = 8
+
+
+# In[]:
+
+
+xgb_r2 = np.round(sklearn.metrics.r2_score(y_true=train[target], y_pred=best_regression.predict(train_x_regression)), 2)
+
+plt.scatter(x=train[target], y=best_regression.predict(train_x_regression), label=f"XGBoost, R2={xgb_r2}", c="black")
+
+markers = iter('+vo^')
+for model_label in ['r1_1term', 'r1_2term', 'r1_3term', 'r1_4term']:
+    plt.scatter(x=train[target], y=train[model_label], label=f"{model_label}, R2={get_r2(model_label, train)}", alpha=0.3, marker=next(markers))
+plt.plot([train_min, train_max], [train_min, train_max], c='k', linestyle='dashed', label='Parity')
+plt.xlim([train_min, train_max])
+plt.ylim([train_min, train_max])
+
+plt.title('Exfoliation Training Set, Rung1 Models')
+plt.ylabel('Predicted Exfoliation Energy (J/m^2)')
+plt.xlabel('Dataset Exfoliation Energy (J/m^2)')
+plt.legend()
+
+
+# In[]:
+
+
+xgb_r2 = np.round(sklearn.metrics.r2_score(y_true=train[target], y_pred=best_regression.predict(train_x_regression)), 2)
+
+plt.scatter(x=train[target], y=best_regression.predict(train_x_regression), label=f"XGBoost, R2={xgb_r2}", c="black")
+
+markers = iter('+vo^')
+for model_label in ['r2_1term', 'r2_2term', 'r2_3term', 'r2_4term']:
+    plt.scatter(x=train[target], y=train[model_label], label=f"{model_label}, R2={get_r2(model_label, train)}", alpha=0.2, marker=next(markers))
+plt.plot([train_min, train_max], [train_min, train_max], c='k', linestyle='dashed', label='Parity')
+plt.xlim([train_min, train_max])
+plt.ylim([train_min, train_max])
+    
+plt.title('Exfoliation Training Set, Rung2 Models')
+plt.ylabel('Predicted Exfoliation Energy (J/m^2)')
+plt.xlabel('Dataset Exfoliation Energy (J/m^2)')
+plt.legend()
+
+
+# In[]:
+
+
+xgb_r2 = np.round(sklearn.metrics.r2_score(y_true=test[target], y_pred=best_regression.predict(test_x_regression)), 2)
+
+plt.scatter(x=test[target], y=best_regression.predict(test_x_regression), label=f"XGBoost, R2={xgb_r2}", c="black")
+
+markers = iter('+vo^')
+for model_label in ['r1_1term', 'r1_2term', 'r1_3term', 'r1_4term']:
+    plt.scatter(x=test[target], y=test[model_label], label=f"{model_label}, R2={get_r2(model_label, test)}", alpha=0.3, marker=next(markers))
+    
+plt.plot([test_min, test_max], [test_min, test_max], c='k', linestyle='dashed', label='Parity')
+plt.xlim([test_min, test_max])
+plt.ylim([test_min, test_max])
+
+plt.title('Exfoliation Test Set, Rung1 Models')
+plt.ylabel('Predicted Exfoliation Energy (J/m^2)')
+plt.xlabel('Dataset Exfoliation Energy (J/m^2)')
+plt.legend()
+
+
+# In[]:
+
+
+xgb_r2 = np.round(sklearn.metrics.r2_score(y_true=test[target], y_pred=best_regression.predict(test_x_regression)), 2)
+
+plt.scatter(x=test[target], y=best_regression.predict(test_x_regression), label=f"XGBoost, R2={xgb_r2}", c="black")
+
+markers = iter('+vo^')
+for model_label in ['r2_1term', 'r2_2term', 'r2_3term', 'r2_4term']:
+    plt.scatter(x=test[target], y=test[model_label], label=f"{model_label}, R2={get_r2(model_label, test)}", alpha=0.3, marker=next(markers))
+plt.plot([test_min, test_max], [test_min, test_max], c='k', linestyle='dashed', label='Parity')
+plt.xlim([test_min, test_max])
+plt.ylim([test_min, test_max])
+
+plt.title('Exfoliation Test Set, Rung2 Models')
+plt.ylabel('Predicted Exfoliation Energy (J/m^2)')
+plt.xlabel('Dataset Exfoliation Energy (J/m^2)')
+plt.legend()
+
