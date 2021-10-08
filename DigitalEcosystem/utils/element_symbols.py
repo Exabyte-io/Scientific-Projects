26f132ffea84046ea5bbdeb5e6ed9fa850f93f78
James Dean <24254612+AcylSilane@users.noreply.github.com>
2021-10-08 14:46:31 -0700

    Fix typo and move element symbol defs to utils

--- DigitalEcosystem/utils/element_symbols.py
+++ DigitalEcosystem/utils/element_symbols.py
@@ -2,3 +2,24 @@ noble_gases = ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn']
 f_block_elements = ['La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
                     'Ac', 'Th', 'Pa', 'U',  'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr']
 synthetic_elements_in_d_block = ['Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+
+bond_length_angle_featurization_defs = {
+    'alkaline' : ['H', 'Li', 'Na', 'K', 'Rb', 'Cs', 'Fr'],
+    'alkaine_earth' : ['Be', 'Mg', 'Ca', 'Sr', 'Ba', 'Ra'],
+    'early_transition' : ['Sc', 'Ti', 'V', 'Cr', 'Mn',
+                          'Y',  'Zr', 'Nb', 'Mo', 'Tc',
+                                'Hf', 'Ta', 'W', 'Re',],
+    'late_transition' : ['Fe', 'Co', 'Ni', 'Cu', 'Zn',
+                         'Ru', 'Rh', 'Pd', 'Ag', 'Cd',
+                         'Os', 'Ir', 'Pt', 'Au', 'Hg'],
+    'triel' : ['B', 'Al', 'Ga', 'In', 'Tl'],
+    'tetrel' : ['C', 'Si', 'Ge', 'Sn', 'Pb'],
+    'pnictogen' : ['N', 'P', 'As', 'Sb', 'Bi'],
+    'chalcogen' : ['O', 'S', 'Se', 'Te', 'Po'],
+    'halide' : ['F', 'Cl', 'Br', 'I', 'At'],
+    'noble_gas' : ['He', 'Ne', 'Ar', 'Kr', 'Xe', 'Rn'],
+    'f_block' : ['La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu',
+                 'Ac', 'Th', 'Pa', 'U'],
+    'post_uranium' : ['Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr',
+                      'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+}
